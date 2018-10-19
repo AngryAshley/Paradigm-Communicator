70eea556b8674af6d41020d54ff3596824b3347b
Daniel-I-Am <djdecloet@gmail.com>
2018-10-19 20:34:40 +0200

    linux - rewrite

--- linux/main.cpp
+++ linux/main.cpp
@@ -1,100 +1,123 @@
-#include <iostream>
-#include <string.h>
+#include <stdio.h>      // standard input / output functions
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
+#include <string.h>     // string function definitions
+#include <unistd.h>     // UNIX standard function definitions
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>    // POSIX terminal control definitions
+#include <iostream>
+#include <thread>
 
 using namespace std;
 
-struct termios tio;
-struct termios stdio;
-struct termios old_stdio;
+int timeout = 250000;
+int USB;
+string readText;
 
-int tty_fd;
+void initConnection() {
+    // open the USB connection `file`
+    USB = open( "/dev/pts/2", O_RDWR| O_NOCTTY );
+    // set the baud speed
+    speed_t baudSpeed = B9600;
 
-void initConnection (char* DEVICE_NAME) {
-    // partly: https://en.wikibooks.org/wiki/Serial_Programming/Serial_Linux#termios
-    tcgetattr(STDOUT_FILENO,&old_stdio);
+    // define a couple new variables
+    struct termios tty;
+    struct termios tty_old;
+    // fill memory with 0s
+    memset (&tty, 0, sizeof tty);
 
-    memset(&stdio,0,sizeof(stdio));
-    stdio.c_iflag=0;
-    stdio.c_oflag=0;
-    stdio.c_cflag=0;
-    stdio.c_lflag=0;
-    stdio.c_cc[VMIN]=1;
-    stdio.c_cc[VTIME]=0;
-    tcsetattr(STDOUT_FILENO,TCSANOW,&stdio);
-    tcsetattr(STDOUT_FILENO,TCSAFLUSH,&stdio);
-    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);       // make the reads non-blocking
-
-    memset(&tio,0,sizeof(tio));
-    tio.c_iflag=0;
-    tio.c_oflag=0;
-    tio.c_cflag=CS8|CREAD|CLOCAL;           // 8n1, see termios.h for more information
-    tio.c_lflag=0;
-    tio.c_cc[VMIN]=1;
-    tio.c_cc[VTIME]=5;
-
-    tty_fd=open(DEVICE_NAME, O_RDWR | O_NONBLOCK);
-    cfsetospeed(&tio,B9600);            // 9600 baud
-    cfsetispeed(&tio,B9600);            // 9600 baud
-
-    tcsetattr(tty_fd,TCSANOW,&tio);
-}
-
-void closeConnection() {
-    close(tty_fd);
-    tcsetattr(STDOUT_FILENO,TCSANOW,&old_stdio);
-}
-
-void sendData(char* data, int bytes) {
-    int spot = 0;
-    do {
-        write(tty_fd,&data[spot],1);
-        spot++;
-    } while (spot < bytes);
-}
-
-void sendData(char* data) {
-    sendData(data, (unsigned)strlen(data));
-}
-
-void readData(char* buf, int amount) {
-    int charRead = 0;
-    char c;
-    do {
-        if (read(tty_fd, &c, 1) > 0) { // check if data was sent
-            strcat(buf, &c); // concat to buffer
-            charRead++;
-        }
-    } while (charRead < amount);
-}
-
-void readDataUntil(char* buf, char endCharacter, unsigned int maximum) {
-    do {
-        readData(buf, 1);
-    } while(buf[strlen(buf)-1] != endCharacter && strlen(buf) < maximum);
-}
-
-int main () {
-    initConnection((char*)"/dev/pts/2");
-    char* buf = new char[255];
-    sendData((char*)"\r\nUsername: ");
-    readDataUntil(buf, (char)'\x0d', 50);
-    if ((string)buf == "user\r") {
-        buf = new char[255];
-        sendData((char*)"\r\nPassword: ");
-        readDataUntil(buf, (char)'\x0d', 50);
-        if ((string)buf == "password\r") {
-            cout << "Success" << endl;
-        } else {
-            cout << "Fail" << endl;
-        }
-    } else {
-        cout << "Fail" << endl;
+    // error handling
+    if ( tcgetattr ( USB, &tty ) != 0 ) {
+       cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << endl;
     }
-    closeConnection();
-    return 1;
+
+    // save old tty parameters
+    tty_old = tty;
+
+    // set Baud Rate
+    cfsetospeed (&tty, baudSpeed);
+    cfsetispeed (&tty, baudSpeed);
+
+    // setting other port stuff
+    tty.c_cflag     &=  ~PARENB;            // Make 8n1
+    tty.c_cflag     &=  ~CSTOPB;
+    tty.c_cflag     &=  ~CSIZE;
+    tty.c_cflag     |=  CS8;
+
+    tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+    tty.c_cc[VMIN]   =  1;                  // read doesn't block
+    tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+    tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+    // make raw
+    cfmakeraw(&tty);
+
+    // flush port, then applies attributes
+    tcflush( USB, TCIFLUSH );
+    if ( tcsetattr ( USB, TCSANOW, &tty ) != 0) {
+       std::cout << "Error " << errno << " from tcsetattr" << std::endl;
+    }
+}
+
+void writeData(string str) {
+    char cmd[str.size() + 1];
+    strcpy(cmd, str.c_str());
+    write( USB, &cmd, sizeof(cmd));
+}
+
+string readData(char replacement) {
+    int n = 0,
+    spot = 0,
+    maxSize = 255;
+    char buf = '\0';
+
+    char response[maxSize];
+    memset(response, '\0', sizeof response);
+
+    do {
+        n = read( USB, &buf, 1 );
+        sprintf( &response[spot], "%c", buf );
+        if (replacement) { writeData(string(1, replacement)); }
+            else { writeData(string(1, buf)); };
+        spot += n;
+    } while( !(buf == '\r' || buf == '\n' || buf == '\0') && n > 0 && spot < maxSize);
+
+    if (n < 0) {
+        // Some error occured
+        return (string)"ERROR!!!";
+    }
+    else if (n == 0) {
+        return (string)"";
+    }
+    return (string)response;
+}
+
+void STDInToSerial () {
+    do {
+        char buf = '\0';
+        read(0, &buf, 1);
+        if ((char)buf == (char)'\r' || (char)buf == (char)'\n')
+            break;
+        usleep(timeout);
+        writeData((string)&buf);
+    } while (true);
+}
+
+void readConfig () {}
+
+void runProgram() {
+    writeData((string)"Username: ");
+    string str = readData(NULL);
+    cout << str << endl;
+}
+
+int main() {
+    initConnection();
+    readConfig();
+    thread x(runProgram);
+    thread y(STDInToSerial);
+
+    x.join();
+    y.join();
+    return 0;
 }
